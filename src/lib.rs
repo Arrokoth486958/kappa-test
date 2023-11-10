4850860eba4ad2b2f8ae338c61aae44d9b4c2a0b
Arrokoth <arrokoth233@gmail.com>
2023-11-10 21:48:06 +0100

    创建渲染器文件

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 // 好哎！是卡帕！
 pub mod app;
-pub mod log;
-pub mod wgpu;
 pub mod error;
+pub mod log;
+pub mod renderer;
+pub mod wgpu;
---
+++ src/renderer.rs
@@ -0,0 +1,5 @@
+pub struct RenderSystem {
+}
+
+impl RenderSystem {
+}
